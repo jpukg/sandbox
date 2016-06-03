685d3a79136b05d6586596af298a5048d64a8187
Jayaprakash <j.prakashuk@gmail.com>
2016-06-03 17:51:25 +0530

    .

---
+++ general-utils/src/main/java/org/example/utils/JpaSchemaExport.java
@@ -0,0 +1,41 @@
+package org.example.utils;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.dialect.HSQLDialect;
+import org.hibernate.dialect.Oracle10gDialect;
+import org.hibernate.ejb.Ejb3Configuration;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.hibernate.tool.hbm2ddl.Target;
+
+@SuppressWarnings("deprecation")
+public class JpaSchemaExport {
+
+  private static final String DIALECT_ORACLE = "ORACLE";
+  private static final String SCHEMA_NAME = "EXAMPLE";
+
+  public static void main(String[] args) throws IOException {
+    execute(DIALECT_ORACLE, "example-unit", "src/test/resources/example-ddl-scripts.sql", false);
+  }
+
+  public static void execute(String dialectName, String persistenceUnitName, String destination, boolean format) {
+    System.out.println("Starting schema export");
+
+    Ejb3Configuration cfg = new Ejb3Configuration().configure(persistenceUnitName, new Properties());
+    cfg.setProperty("hibernate.dialect", HSQLDialect.class.getName());
+    if (DIALECT_ORACLE.equalsIgnoreCase(dialectName)) {
+      cfg.setProperty("hibernate.dialect", Oracle10gDialect.class.getName());
+    }
+    cfg.setProperty("hibernate.default_schema", SCHEMA_NAME);
+    Configuration hbmcfg = cfg.getHibernateConfiguration();
+    SchemaExport schemaExport = new SchemaExport(hbmcfg);
+    schemaExport.setOutputFile(destination);
+    schemaExport.setDelimiter(";");
+    schemaExport.setFormat(format);
+    schemaExport.execute(Target.SCRIPT, SchemaExport.Type.BOTH);
+
+    System.out.println("Schema exported to " + destination);
+  }
+}
